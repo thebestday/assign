ddb750a19d5fd45e21dac83ca8b1a1e2bb9b6f14
For Web <bestweb@europe.com>
2018-11-26 22:46:36 +0500

    First

---
+++ Assignment1/script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+let money = prompt('Ваш бюджет на месяц?', ''),
+    time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+
+let appData = {
+    budget: money,    
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    timeData: time,
+    savings: false
+};
+
+let a1 =  prompt('Введите обязательную статью расходов в этом месяце', ''), 
+    a2 =  prompt('Во сколько обойдется?', ''),
+    a3 =  prompt('Введите обязательную статью расходов в этом месяце', ''),
+    a4 =  prompt('Во сколько обойдется?', '');
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+
+console.log(appData);
+
+
+alert( "бюджет пользователя на 1 день : " + (appData.budget / 30));
